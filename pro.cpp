b1b064a9cfd8647b70bae947f4f481fd3d5a807f
Kelsit0 <ramosreynagaoscar6gmail.com>
2023-06-19 10:57:18 -0600

    Se hizo la func2

--- pro.cpp
+++ pro.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 void metodo();
+void metodo2();
+
 using namespace std;
 int main(){
     metodo();    ///se llama al metodo
@@ -8,4 +10,7 @@ int main(){
 
 void metodo(){
     cout<<"oa cabrones"<<endl;
+}
+void metodo2(){
+    cout<<"chupapi"<<endl;
 }
