a657befccb5c4aea71667faa4d142eee0be6353b
Kelsit0 <ramosreynagaoscar6@gmail.com>
2023-06-21 17:56:55 -0600

    se hizo la funcion suma con exito

--- pro.cpp
+++ pro.cpp
@@ -1,15 +1,41 @@
 #include<iostream>
-void metodo();
+int suma(int,int);
 void metodoDefinitivo();
 
 using namespace std;
 int main(){
-    metodo();    ///se llama al metodo
+    char opt;
+    int a=0,b=0;
+    do{
+        cout<<"1)Suma"<<endl;
+        cout<<"2)Resta"<<endl;
+        cout<<"3)Multp"<<endl;
+        cout<<"4)Div"<<endl;
+        cout<<"s)Salir"<<endl;
+        cout<<endl;
+        cin>>opt;
+            switch(opt){
+            case '1':
+                cout<<"Ingresa el 1er num"<<endl;
+                cin>>a;
+                cout<<"Ingresa el 2do num"<<endl;
+                cin>>b;
+                cout<<"resultado: "<<suma(a,b);
+                cout<<endl;
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+        }
+    }while(opt!='s');
     return 0;
 }
 
-void metodo(){
-    cout<<"oa cabrones"<<endl;
+int suma(int a, int b){
+    return a+b;
 }
 void metodoDefinitivo(){
     cout<<"chupapi"<<endl;
